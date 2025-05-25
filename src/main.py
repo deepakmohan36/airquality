22b3a77a397337ca66e3fce38c17dd118011ac47
dataguydeepak <deepak_mohan@condenast.com>
2025-05-25 22:45:33 +0530

    add location and location details extraction scripts

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from locations import get_all_location_data
-from air_quality_data import get_airquality_data
+from location_details import get_location_details_for_multiple_locations
 from load_data import load_data_to_bigquery_clear_table
 
 
 ids_list, locations_list = get_all_location_data()
 load_data_to_bigquery_clear_table('raw_airquality','raw_locations', locations_list)
 
-air_quality_data = get_airquality_data(ids_list)
-load_data_to_bigquery_clear_table('raw_airquality','raw_airquality', air_quality_data)
+location_details = get_location_details_for_multiple_locations(ids_list)
+load_data_to_bigquery_clear_table('raw_airquality','raw_location_details', location_details)
