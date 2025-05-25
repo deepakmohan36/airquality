ea3781d30b9ac5214ac750734991f70fdaf1e5ae
dataguydeepak <deepak_mohan@condenast.com>
2025-05-25 15:02:07 +0530

    fix get_location rate limit issue by adding delays and retries

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from locations import get_all_location_data
 from air_quality_data import get_airquality_data
 from load_data import load_data_to_bigquery_clear_table
 
-air_quality_data = get_airquality_data('8118')
+
+ids_list, locations_list = get_all_location_data()
+load_data_to_bigquery_clear_table('raw_airquality','raw_locations', locations_list)
+
+air_quality_data = get_airquality_data(ids_list)
 load_data_to_bigquery_clear_table('raw_airquality','raw_airquality', air_quality_data)
