d4fbcc8663faa50426558a032e73dfad85b8848c
dataguydeepak <deepak_mohan@condenast.com>
2025-05-26 11:14:00 +0530

    add parameters extraction script

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+from parameters import get_all_parameters
 from locations import get_all_location_data
 from location_details import get_location_details_for_multiple_locations
 from load_data import load_data_to_bigquery_clear_table
 
+parameters = get_all_parameters()
+load_data_to_bigquery_clear_table('raw_airquality','raw_parameters', parameters)
 
 ids_list, locations_list = get_all_location_data()
 load_data_to_bigquery_clear_table('raw_airquality','raw_locations', locations_list)
